d2b45b22ddd89c435141f3de2d1ba22f086b2f2a
Philippe Creignou <pcreignou38@gmail.com>
2025-02-03 16:51:53 +0100

    outpout token

--- src/services/archive.service.ts
+++ src/services/archive.service.ts
@@ -1,9 +1,12 @@
 import path from 'path';
 import { ArchiveFileBody, ArchiveFileResponse } from '../models/archive';
-import fs from 'fs';
 import { IReq, IRes } from '../utils/types';
+import axios, { AxiosResponse } from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
 
-export const archiveFile = (req: IReq<ArchiveFileBody>, res: IRes) => {
+
+export const archiveFile = async (req: IReq<ArchiveFileBody>, res: IRes) => {
   const {
     body: {
       files: [file],
@@ -13,15 +16,49 @@ export const archiveFile = (req: IReq<ArchiveFileBody>, res: IRes) => {
   console.log( '### storing: '+ file.name);
   console.log( '### Values: '+ file.pathTemplateValues)
   const fileBuffer = Buffer.from(file.content, 'base64');
+  const HUBSPOT_API_KEY ='7544524';
+
+
   try {
     if (file.path) {
       fs.mkdirSync(file.path, { recursive: true });
     }
     fs.writeFileSync(path.join(file.path, file.name), fileBuffer);
+    
+      // Initialize FormData to send file as multipart form data
+      const form = new FormData();
+  
+      // Create a readable stream from the file path
+      const fileStream = fs.createReadStream(file.path);
+  
+      // Append file to the form data with 'file' as the key
+      form.append('file', fileStream, {
+        filename: file.name, // Optional: Specify the filename
+        contentType: file.contentType, // Optional: Specify content type
+      });
+  
+      // HubSpot File Upload API endpoint
+      const url = `https://api.hubapi.com/filemanager/api/v3/files/upload?hapikey=${HUBSPOT_API_KEY}`;
+  
+      // Make the POST request to upload the file
+      const response: AxiosResponse = await axios.post(url, form, {
+        headers: {
+          'Authorization':req.headers.authorization,
+        },
+      });
+  
+      console.log('File uploaded successfully:', response.data); 
+
+  
+
     const archiveResult: ArchiveFileResponse = { message: 'Successful archive' };
     return res.json(archiveResult);
   } catch (err) {
-    console.log(`Encountered an error archiving file: ${err.message}`);
+    if (axios.isAxiosError(err)) {
+      console.error('Error uploading file:', err.response?.data || err.message);
+    } else {
+      console.log(`Encountered an error archiving file: ${err.message}`)
+    };
     const archiveResult: ArchiveFileResponse = { message: 'Failed archive' };
     return res.json(archiveResult);
   }
